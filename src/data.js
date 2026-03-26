672a2cd38cbf5636c4b49c0ca9281250a4bf8279
John Paul Ohakwe <johnpaulcom49@gmail.com>
2026-03-26 16:21:17 +0100

    tech

--- src/data.js
+++ src/data.js
@@ -19,7 +19,7 @@ export const developers = [
       {
         name: "Food Restaurant Website",
         desc: "A restaurant landing page with menu and contact section.",
-        tech: ["HTML", "CSS", "JavaScript"]
+        tech: ["HTML", "CSS", "JavaScrip"]
       }
     ],
     links: {
